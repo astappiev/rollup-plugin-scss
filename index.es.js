771518b36bb49731e37257af315c71add80b82df
Oleh Astappiev <astappiev@l3s.de>
2021-01-23 20:01:28 +0100

    bugfixes and improved map when using PostCSS

--- index.es.js
+++ index.es.js
@@ -33,7 +33,7 @@ export default function css (options = {}) {
         }, options))
 
         const css = render.css.toString()
-        const map = render.map ? render.map.toString() : null;
+        const map = render.map ? render.map.toString() : null
 
         // Possibly process CSS (e.g. by PostCSS)
         if (typeof options.processor === 'function') {
@@ -41,8 +41,8 @@ export default function css (options = {}) {
 
           // PostCSS support
           if (typeof processor.process === 'function') {
-            return Promise.resolve(processor.process(css, { from: undefined }))
-              .then(result => result.css)
+            return Promise.resolve(processor.process(css, { from: undefined, map: { prev: map, inline: false } }))
+              .then(result => ({ css: result.css, map: result.map ? result.map.toString() : null }))
           }
 
           return processor
@@ -90,8 +90,8 @@ export default function css (options = {}) {
 
       // When output is disabled, the stylesheet is exported as a string
       if (options.output === false) {
-        return Promise.resolve(compileSass(code).css).then(css => ({
-          code: 'export default ' + JSON.stringify(css),
+        return Promise.resolve(compileSass(code)).then(compiled => ({
+          code: 'export default ' + JSON.stringify(compiled.css),
           map: { mappings: '' }
         }))
       }
@@ -126,13 +126,13 @@ export default function css (options = {}) {
 
       // Resolve if processor returned a Promise
       Promise.resolve(compiled).then(compiled => {
-        // Emit styles through callback
-        if (typeof options.output === 'function') {
-          options.output(compiled.css, styles)
+        if (typeof compiled.css !== 'object' || typeof compiled.css !== 'string') {
           return
         }
 
-        if (typeof compiled.css !== 'string') {
+        // Emit styles through callback
+        if (typeof options.output === 'function') {
+          options.output(compiled.css, styles)
           return
         }
 
@@ -158,9 +158,9 @@ export default function css (options = {}) {
         if (compiled.map) {
           writeFile(dest + '.map', compiled.map, (err) => {
             if (opts.verbose !== false && err) {
-              console.error(red(err));
+              console.error(red(err))
             }
-          });
+          })
         }
       })
     }
